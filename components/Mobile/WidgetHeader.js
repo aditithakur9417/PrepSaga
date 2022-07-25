6398062a66e677cc915161131820fa5667607255
Aayush-IGP <aayush.mundhra@igp.com>
2022-07-25 10:24:25 +0530

    adding small fixes

--- components/Mobile/WidgetHeader.js
+++ components/Mobile/WidgetHeader.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function WidgetHeader({framesData}) {
   return (
-    <div className={`widgetTileLinkHeader ${framesData.url && "notViewAll"}`}>
+    <div className={`widgetTileLinkHeader ${!framesData.url && "notViewAll"}`}>
         <div>
-        {framesData.url ? <a href={framesData.url} className="widgetTitleText">{framesData.title}</a> :<div className="widgetTitleText">{framesData.title}</div>}
-          <div className="widgetSubtitleText">{framesData.subTitle}</div>
+        {framesData.title ? (framesData.url ? <a href={framesData.url} className="widgetTitleText">{framesData.title}</a> :<div className="widgetTitleText">{framesData.title}</div>) : null}
+        {framesData.subTitle ? <div className="widgetSubtitleText">{framesData.subTitle}</div> : null}
         </div>
         {framesData.url ? <a href={framesData.url} className="widgetTitleLink">{framesData.viewAll}</a> : null}
     </div>
