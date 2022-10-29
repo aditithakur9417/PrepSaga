8f875b1e811f3b672080d24e00c9195063c4a0cf
aditithakur9417 <aditithakur1425@gmail.com>
2022-10-29 14:10:36 +0530

    chatEngine testing

--- pages/Approutes.js
+++ pages/Approutes.js
@@ -1,12 +1,13 @@
 import CompaniesGuide from "../components/Mobile/CompaniesGuide";
 import LanguagesGuide from "../components/Mobile/LanguagesGuide";
+import AblyChatComponent from "../components/AblyChatComponent";
 import { homeData } from "../data/homeData";
 export default function Approutes() {
  
 
   return (
     <>
-<CompaniesGuide/>
+<AblyChatComponent/>
 <LanguagesGuide/>
     </>
   );
